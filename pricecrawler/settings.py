38ad7473e80c95f95430b3f09c6806dbf72489df
Ryan Hernandez <ryan.d.hernandez@ttu.edu>
2022-05-25 22:53:47 -0500

    Update settings.py

--- pricecrawler/settings.py
+++ pricecrawler/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o9vnsw0ob^@_0dk(d*2li(whs!i7c9_j@p=wl3m5q(sv_$%44z'
+SECRET_KEY = 'Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
